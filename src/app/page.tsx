68a18fc5c5f31adc3cbe5c073350973dc76f13df
Mounesha Zonde <mouneshe2002@gmail.com>
2024-08-19 02:16:38 +0530

    finished rest of the pages and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+import Footer from "@/components/Footer";
+import Instructors from "@/components/Instructors";
+import Testimonials from "@/components/Testimonials";
 import FeaturedCourses from "@/components/ui/FeaturedCourses";
 import HeroSection from "@/components/ui/HeroSection";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white">
+    <main className="min-h-screen ">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
+      <Testimonials />
+      <UpcomingWebinar />
+      <Instructors />
+      <Footer />
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,27 +2,38 @@
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
+import Link from "next/link";
 
 export default function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
-    >
+      className={cn(
+        "fixed top-10 inset-x-0 max-w-2xl mx-auto z-50",
+        className
+      )}>
       <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Home">
-        </MenuItem>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"></MenuItem>
+        </Link>
         <MenuItem setActive={setActive} active={active} item="Our Courses">
-        <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">All Courses</HoveredLink>
-            <HoveredLink href="/interface-design">Basic Music Theory</HoveredLink>
-            <HoveredLink href="/seo">Advanced Composition</HoveredLink>
-            <HoveredLink href="/branding">Song Writing</HoveredLink>
-            <HoveredLink href="/branding">Music Production</HoveredLink>
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="#">Basic Music Theory</HoveredLink>
+            <HoveredLink href="#">Advanced Composition</HoveredLink>
+            <HoveredLink href="#">Song Writing</HoveredLink>
+            <HoveredLink href="#">Music Production</HoveredLink>
           </div>
         </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Contact Us">
-        </MenuItem>
+        <Link href={"/contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"></MenuItem>
+        </Link>
       </Menu>
     </div>
   );
